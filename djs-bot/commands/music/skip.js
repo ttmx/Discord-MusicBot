851a4e1c3652df5233e9bb1f8929871ccdaa8dd9
Timofei Galkin <t.galkin@group-ib.com>
2024-01-12 13:06:37 +0300

    fix(skip): wrong redEmbed call

--- djs-bot/commands/music/skip.js
+++ djs-bot/commands/music/skip.js
@@ -43,20 +43,23 @@ const command = new SlashCommand()
 		if (status === 1) {
 			return interaction.reply({
 				embeds: [
-					redEmbed(
-						`There is nothing after [${song.title}](${song.uri}) in the queue.`
-					),
+					redEmbed({
+						desc: `There is nothing after [${song.title}](${song.uri}) in the queue.`
+					}),
 				],
 			});
 		}
 
-		interaction.reply({
+		const ret = await interaction.reply({
 			embeds: [
 				new EmbedBuilder()
 					.setColor(client.config.embedColor)
 					.setDescription("✅ | **Skipped!**"),
 			],
+		 	fetchReply: true 
 		});
+		if (ret) setTimeout(() => ret.delete().catch(client.warn), 20000);
+		return ret;
 	});
 
 module.exports = command;
