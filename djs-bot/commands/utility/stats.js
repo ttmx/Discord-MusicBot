96614b137226e03b589d8266f8e8148fa0c55c0f
birb <imanimtiyaz242@gmail.com>
2024-03-08 16:12:38 +0800

    fix: stats would only show the first node
    this will fix where `/stats` only show the first node even though the bot were connected to more than one node. (#173)

--- djs-bot/commands/utility/stats.js
+++ djs-bot/commands/utility/stats.js
@@ -8,37 +8,30 @@ const command = new SlashCommand()
 	.setName("stats")
 	.setDescription("Get information about the bot")
 	.setRun(async (client, interaction) => {
+		let nodesInfo = "";
+
+		client.manager.Engine.nodes.forEach((node) => {
+			// show lavalink uptime in a nice format
+			const lavauptime = moment
+				.duration(node.stats.uptime)
+				.format(" D[d], H[h], m[m]");
+			// show lavalink memory usage in a nice format
+			const lavaram = (node.stats.memory.used / 1024 / 1024).toFixed(2);
+
+			nodes += `\`\`\`yml\nNode: ${node.options.identifier}\nUptime: ${lavauptime}\nRAM: ${lavaram} MB\nPlayers: ${node.stats.playingPlayers} out of ${node.stats.players}\nWrapper: ${client.config.musicEngine}\`\`\`\n`;
+		});
+
 		// get OS info
 		const osver = os.platform() + " " + os.release();
-		
+
 		// Get nodejs version
 		const nodeVersion = process.version;
-		
-		// get the uptime in a human readable format
-		const runtime = moment
-			.duration(client.uptime)
-			.format("d[ Days]・h[ Hrs]・m[ Mins]・s[ Secs]");
-		// show lavalink uptime in a nice format
-		const lavauptime = moment
-			.duration(client.manager.Engine.nodes.values().next().value.stats.uptime)
-			.format(" D[d], H[h], m[m]");
-		// show lavalink memory usage in a nice format
-		const lavaram = (
-			client.manager.Engine.nodes.values().next().value.stats.memory.used /
-			1024 /
-			1024
-		).toFixed(2);
-		// sow lavalink memory alocated in a nice format
-		const lavamemalocated = (
-			client.manager.Engine.nodes.values().next().value.stats.memory.allocated /
-			1024 /
-			1024
-		).toFixed(2);
+
 		// show system uptime
-		var sysuptime = moment
+		const sysuptime = moment
 			.duration(os.uptime() * 1000)
 			.format("d[ Days]・h[ Hrs]・m[ Mins]・s[ Secs]");
-		
+
 		// get commit hash and date
 		let gitHash = "unknown";
 		try {
@@ -50,41 +43,36 @@ const command = new SlashCommand()
 			// do nothing
 			gitHash = "unknown";
 		}
-		
+
 		const statsEmbed = new EmbedBuilder()
-			.setTitle(`${ client.user.username } Information`)
+			.setTitle(`${client.user.username} Information`)
 			.setColor(client.config.embedColor)
 			.setDescription(
-				`\`\`\`yml\nName: ${ client.user.username }#${ client.user.discriminator } [${ client.user.id }]\nAPI: ${ client.ws.ping }ms\nRuntime: ${ runtime }\`\`\``,
+				`\`\`\`yml\nName: ${client.user.username}#${client.user.discriminator} [${client.user.id}]\nAPI: ${client.ws.ping}ms\n\`\`\``
 			)
 			.setFields([
 				{
 					name: `Lavalink stats`,
-					value: `\`\`\`yml\nUptime: ${ lavauptime }\nRAM: ${ lavaram } MB\nPlaying: ${
-						client.manager.Engine.nodes.values().next().value.stats.playingPlayers
-					} out of ${
-						client.manager.Engine.nodes.values().next().value.stats.players
-					}\nWrapper: ${
-						client.config.musicEngine
-					}\`\`\``,
-					inline: true,
+					value: nodes,
+					inline: false,
 				},
 				{
 					name: "Bot stats",
 					value: `\`\`\`yml\nGuilds: ${
 						client.guilds.cache.size
-					} \nNodeJS: ${ nodeVersion }\nDiscordMusicBot: v${
+					} \nNodeJS: ${nodeVersion}\nDiscordMusicBot: v${
 						require("../../package.json").version
 					} \`\`\``,
 					inline: true,
 				},
 				{
 					name: "System stats",
-					value: `\`\`\`yml\nOS: ${ osver }\nUptime: ${ sysuptime }\n\`\`\``,
+					value: `\`\`\`yml\nOS: ${osver}\nUptime: ${sysuptime}\n\`\`\``,
 					inline: false,
 				},
 			])
-			.setFooter({ text: `Build: ${ gitHash }` });
+			.setFooter({ text: `Build: ${gitHash}` });
+
 		return interaction.reply({ embeds: [statsEmbed], ephemeral: false });
 	});
 
