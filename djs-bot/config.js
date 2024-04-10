ttmx/Discord-MusicBot

commit 2687039ee764611e1f1628335132ec07a5aac971

bot ws integration

djs-bot/config.js
@@ -43,7 +43,17 @@ module.exports = {
 		/**
 		 * Port to run the API on
 		 * @type {number} */
-		port: parseInt(process.env.API_PORT) || 8080,
+		port: parseInt(process.env.API_PORT) || 3000,
+	},
+
+	/**
+	 * WS configuration
+	 */
+	ws: {
+		/**
+		 * Port to run WS on
+		 * @type {number} */
+		port: parseInt(process.env.WS_PORT) || 8080,
 	},
 
 	/**

djs-bot/.env.example
@@ -24,4 +24,5 @@ DATABASE=""
 DATABASE_URL="postgresql://root:root@postgres-db:5432/base?schema=public"
 
 # Backend configuration
-API_PORT=8080
+API_PORT=3000
+WS_PORT=8080

djs-bot/api/v1/src/utils/ws.ts
@@ -3,3 +3,7 @@ import { WS_ROUTES_PREFIX } from '../lib/constants';
 export function createWsRoute(route: string) {
   return WS_ROUTES_PREFIX + route;
 }
+
+export function bufferToString(buf: ArrayBuffer) {
+  return Buffer.from(buf).toString();
+}

djs-bot/api/v1/src/ws/index.ts
@@ -1,6 +1,6 @@
 import { WSApp } from '../interfaces/common';
 import { wsLog } from '../utils/log';
-import { createWsRoute } from '../utils/ws';
+import { bufferToString, createWsRoute } from '../utils/ws';
 import { getPkg } from '../';
 
 export function setupWsServer(wsServer: WSApp) {
@@ -14,18 +14,27 @@ export function setupWsServer(wsServer: WSApp) {
       // pong: (ws, message) => {},
 
       open: (ws) => {
-        wsLog('log', 'echo: Connected:', ws.getRemoteAddressAsText.toString());
+        wsLog(
+          'log',
+          'echo: Connected:',
+          bufferToString(ws.getRemoteAddressAsText()),
+        );
       },
       message: (ws, message, isBinary) => {
-        wsLog('log', 'echo: Message:', ws.getRemoteAddressAsText.toString());
+        wsLog(
+          'log',
+          'echo: From:',
+          bufferToString(ws.getRemoteAddressAsText()),
+        );
+
+        wsLog('log', 'echo: Message:', bufferToString(message));
 
         let ok = ws.send(message, isBinary);
 
         wsLog('log', 'echo: Status:', ok);
       },
       close: (ws, code, message) => {
-        wsLog('log', 'echo: Close:', ws.getRemoteAddressAsText.toString());
-        wsLog('log', 'echo: Message:', message.toString());
+        wsLog('log', 'echo: Message:', bufferToString(message));
         wsLog('log', 'echo: Code:', code);
       },
     })

djs-bot/lib/Bot.d.ts
@@ -8,7 +8,7 @@ import getChannel from "../util/getChannel";
 import getLavalink from "../util/getLavalink";
 import { CosmiTrack } from "cosmicord.js";
 import config from "../config";
-import { app } from "../api/v1/src";
+import { app, wsApp } from "../api/v1/src";
 
 /**
  * The class groups some useful functions for the client in order to facilitate expandability, maintenance and manageability
@@ -32,6 +32,7 @@ declare class Bot extends Client {
 	denom: string | undefined;
 	manager: MusicManager | undefined;
 	api: ReturnType<typeof app>;
+	wsServer: ReturnType<typeof wsApp>;
 	db: DBMS | undefined;
 	commandsRan: number;
 	songsPlayed: number;

djs-bot/lib/Bot.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const path = require("path");
 const DBMS = require("./DBMS");
 const Logger = require("./Logger");
-const { app } = require("../api/v1/dist");
+const { app, wsApp } = require("../api/v1/dist");
 const getConfig = require("../util/getConfig");
 const MusicManager = require("./MusicManager");
 
@@ -143,6 +143,7 @@ class Bot extends Client {
 
 		// API initialization (done after the login to prevent lack of info)
 		this.api = app(this);
+		this.wsServer = wsApp();
 
 		// DBMS initialization (done after the login to prevent lack of info)
 		if (this.config.db_url && this.config.db_url !== "") {

djs-bot/loaders/events/ready.js
@@ -37,5 +37,14 @@ module.exports = (client) => {
 		client.info(`API is now listening on port ${client.config.api.port}`);
 	});
 
+	client.wsServer.listen(client.config.ws.port, (sock) => {
+		if (sock) {
+			client.info(`WS is now listening on port ${client.config.ws.port}`);
+			return;
+		}
+
+		client.error(new Error("Can't start WS"));
+	});
+
 	client.info("Successfully logged in as " + client.user.tag);
 };

commit f7172b32842066a98e51d046d9def5284134f6c8

djs-bot: fix mismatched port API and WS (!214)
WS and API port interverted in config.js default #210
In the config.js file, the default port for api and ws (line 46 and 56) are interverted. The ws' port is 8080 instead of 3000 and the other way around.

Fixes: 2687039ee7 ("bot ws integration")
Signed-off-by: Darren Nathanael <github@darrennathanael.com>
Link: https://git.dpaste.org/dmb/Discord-MusicBot/commit/2687039ee764611e1f1628335132ec07a5aac971

Reviewed-on: https://git.dpaste.org/dmb/Discord-MusicBot/pulls/214
Co-authored-by: Darren Nathanael <github@darrennathanael.com>
Co-committed-by: Darren Nathanael <github@darrennathanael.com>

djs-bot/config.js
@@ -43,7 +43,7 @@ module.exports = {
 		/**
 		 * Port to run the API on
 		 * @type {number} */
-		port: parseInt(process.env.API_PORT) || 3000,
+		port: parseInt(process.env.API_PORT) || 8080,
 	},
 
 	/**
@@ -53,7 +53,7 @@ module.exports = {
 		/**
 		 * Port to run WS on
 		 * @type {number} */
-		port: parseInt(process.env.WS_PORT) || 8080,
+		port: parseInt(process.env.WS_PORT) || 3000,
 	},
 
 	/**